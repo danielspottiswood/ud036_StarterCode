f49b9312282f4bf12e4c8e6a5627eaec1b9b1349
danielspottiswood <danielspottiswood@gmail.com>
2017-05-16 14:57:01 -0700

    Update media.py

--- media.py
+++ media.py
@@ -1,11 +1,23 @@
 import webbrowser
+
+
 class Movie():
-    #The function below initializes the Movie object with the following traits, and sets them to be equal to that of the object.
-    def __init__(self, movie_title, movie_storyline, poster_image_url, trailer_youtube):
+# This class defines a movie with the four traits described below.
+# It has a function that shows the trailer of the movie from the web.
+
+    def __init__(
+        self, movie_title, movie_storyline, poster_image_url, trailer_youtube_url):
+        """
+        Initializes the Movie object with the following traits,
+        and sets them to be equal to that of the object.
+        """
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image_url
-        self.trailer_youtube_id = trailer_youtube
-    #This function opens the trailer of the specific movie.
+        self.trailer_youtube_url = trailer_youtube_url
+        
     def show_trailer(self):
+        """
+        Opens the trailer of the specific movie.
+        """
         webbrowser.open(self.trailer_youtube_url)
